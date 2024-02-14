dc5a734d51974a08e79024b1ece308d6c1e9549f
youngil <youngil@placeb.com>
2024-02-14 10:05:52 +0900

    행동 트리 태스크 및 시퀀스

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -13,8 +13,9 @@ void AShooterAIController::BeginPlay()
 	{
 		RunBehaviorTree(AIBehavior);
 
-		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
 
+		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);		
 		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
 	}
 }
