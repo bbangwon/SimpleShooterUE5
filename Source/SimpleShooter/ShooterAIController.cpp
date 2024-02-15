256a9b16b3ca7b071c22f1e6e945b2fa25a525c7
youngil <youngil@placeb.com>
2024-02-15 10:00:06 +0900

    BT 데코레이터 셀렉터

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -24,15 +24,14 @@ void AShooterAIController::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
-	//APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	//if (LineOfSightTo(PlayerPawn))
-	//{
-	//	MoveToActor(PlayerPawn, AcceptanceRadius);
-	//	SetFocus(PlayerPawn);
-	//}
-	//else
-	//{
-	//	ClearFocus(EAIFocusPriority::Gameplay);
-	//	StopMovement();
-	//}
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (LineOfSightTo(PlayerPawn))
+	{
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());		
+	}
+	else
+	{		
+		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+	}
 }
