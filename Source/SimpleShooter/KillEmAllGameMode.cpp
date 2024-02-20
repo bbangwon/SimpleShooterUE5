f3d3b0e62c745a513c382354ed65b3a719c9e9d2
youngil <youngil@placeb.com>
2024-02-20 09:51:28 +0900

    액터 반복하기

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -2,14 +2,25 @@
 
 
 #include "KillEmAllGameMode.h"
+#include "EngineUtils.h"
+#include "GameFramework/Controller.h"
 
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
-	Super::PawnKilled(PawnKilled);
+	Super::PawnKilled(PawnKilled);	
 
 	APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
 	if (PlayerController != nullptr)
 	{
-		PlayerController->GameHasEnded();
+		EndGame(false);
+	}
+}
+
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
+{
+	for (AController* Controller : TActorRange<AController>(GetWorld()))
+	{
+		bool bIsWinner = Controller->IsPlayerController() == bIsPlayerWinner;
+		Controller->GameHasEnded(Controller->GetPawn(), bIsWinner);
 	}	
 }
